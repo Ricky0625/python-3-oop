72f29cbc6f206df7eddde1c7daa1d3f31162df2d
Ricky0625 <rickywong0625@gmail.com>
2023-09-25 20:59:02 +0800

    done ex04

---
+++ ex04/ft_calculator.py
@@ -0,0 +1,20 @@
+class calculator:
+
+    """calculator"""
+
+    @staticmethod
+    def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """dot product"""
+        res = sum(a * b for a, b in zip(V1, V2))
+        print(f"Dot product is: {res}")
+
+    @staticmethod
+    def add_vec(V1: list[float], V2: list[float]) -> None:
+        res = [float(a + b) for a, b in zip(V1, V2)]
+        print(f"Add Vector is: {res}")
+
+    @staticmethod
+    def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """subtract two vector"""
+        res = [float(a - b) for a, b in zip(V1, V2)]
+        print(f"Sous Vector is: {res}")
