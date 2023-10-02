c9dee8e06b6f82f3cf9d25e31d162ff18726529c
Ricky0625 <rickywong0625@gmail.com>
2023-10-02 14:38:28 +0800

    added docstring

--- ex02/DiamondTrap.py
+++ ex02/DiamondTrap.py
@@ -7,31 +7,21 @@ class King(Baratheon, Lannister):
     """A hybrid monster."""
 
     def __init__(self, first_name: str, is_alive: bool = True) -> None:
-
         """Initialize a king"""
-
         super().__init__(first_name, is_alive)
 
     def set_eyes(self, eyes_color: str):
-
         """Set king's eye color"""
-
         self.eyes = eyes_color
 
     def set_hairs(self, hair_color: str):
-
         """Set king's hair color"""
-
         self.hairs = hair_color
 
     def get_eyes(self) -> str:
-
         """Get king's eye color"""
-
         return self.eyes
 
     def get_hairs(self) -> str:
-
         """Get king's hair color"""
-
         return self.hairs
--- ex02/S1E7.py
+++ ex02/S1E7.py
@@ -6,18 +6,19 @@ class Baratheon(Character):
     """Representing the Baratheon family. Implements Character."""
 
     def __init__(self, first_name: str, is_alive: bool = True) -> None:
-
         """Initialize a Baratheon family member"""
-
-        super().__init__(first_name, is_alive)
+        self.first_name = first_name
+        self.is_alive = is_alive
         self.family_name = "Baratheon"
         self.eyes = "brown"
         self.hairs = "dark"
 
     def __repr__(self) -> str:
+        """repr of Baratheon"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def __str__(self) -> str:
+        """str of Baratheon"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
 
@@ -26,19 +27,21 @@ class Lannister(Character):
     """Representing the Lannister family. Implements Character."""
 
     def __init__(self, first_name: str, is_alive: bool = True) -> None:
-
         """Initialize a Lannister family member"""
-
-        super().__init__(first_name, is_alive)
+        self.first_name = first_name
+        self.is_alive = is_alive
         self.family_name = "Lannister"
         self.eyes = "blue"
         self.hairs = "light"
 
     def __repr__(self) -> str:
+        """repr of Lannister"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def __str__(self) -> str:
+        """str of Lannister"""
         return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
 
     def create_lannister(first_name: str, is_alive: bool = True):
+        """create a lannister"""
         return Lannister(first_name, is_alive)
