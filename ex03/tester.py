feab59b4fef51279df2da07dab22d21c025bd370
Ricky0625 <rickywong0625@gmail.com>
2023-10-02 14:40:21 +0800

    updated tester

--- ex03/tester.py
+++ ex03/tester.py
@@ -3,12 +3,6 @@ from ft_calculator import calculator
 
 v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 v1 + 5
-v1 * 5
-v1 - 5
-v1 / 5
-
-v1 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
-v1 + 5
 print("---")
 v2 = calculator([0.0, 1.0, 2.0, 3.0, 4.0, 5.0])
 v2 * 5
